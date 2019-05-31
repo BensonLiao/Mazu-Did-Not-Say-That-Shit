123fcf6de0fbef609656f0121a236a19147be36c
BensonLiao <skylock777@gmail.com>
2019-05-31 17:50:11 +0800

    Change FeedbackSummary component's display text color to light gray

--- src/pages/components/FeedbackSummary.js
+++ src/pages/components/FeedbackSummary.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
 import styled from 'styled-components'
+import { cssVar } from '../styles/variables'
 import { displayFlex, justifyContentSpaceBetween } from '../styles/page'
 import ReactionSummary from './ReactionSummary'
 import CommentAndShareSummary from './CommentAndShareSummary'
@@ -8,6 +9,7 @@ import CommentAndShareSummary from './CommentAndShareSummary'
 const FeedbackSummaryWrapper = styled.div`
   ${displayFlex}
   ${justifyContentSpaceBetween}
+  color: ${cssVar.infoColorGray}
   border-bottom: 1px solid #dadde1;
   margin: 10px 12px 0 12px;
   padding: 0 0 10px 0;
