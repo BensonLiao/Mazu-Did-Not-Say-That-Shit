3c4de48a4f34bb9c1e67bb8e84b515a26666f88e
BensonLiao <skylock777@gmail.com>
2019-07-06 13:07:32 +0800

    Add override style for tooltip of PrivacyIcon

--- src/components/PrivacyIcon.js
+++ src/components/PrivacyIcon.js
@@ -2,23 +2,27 @@ import React from 'react'
 import styled from 'styled-components'
 import StyledTooltip from './StyledTooltip'
 import cssConst from '../styles/constants'
-import { privacyIconStyle } from '../styles/post'
+import { styledTooltipOverrideStyle, privacyIconStyle } from '../styles/post'
 
-const PrivacyIconWrapper = styled.i`
+const PrivacyIconWrapper = styled.div`
+  ${styledTooltipOverrideStyle}
+`
+
+const Icon = styled.i`
   ${privacyIconStyle}
 `
 
 const PrivacyIcon = () => {
   const tooltipId = 'tip-for-post-privacy'
   return (
-    <React.Fragment>
-      <PrivacyIconWrapper data-for={tooltipId} data-tip="公開" />
+    <PrivacyIconWrapper>
+      <Icon data-for={tooltipId} data-tip="公開" />
       <StyledTooltip
         id={tooltipId}
         effect="solid"
         bg={cssConst.tooltipBackgroundBlack}
       />
-    </React.Fragment>
+    </PrivacyIconWrapper>
   )
 }
 
