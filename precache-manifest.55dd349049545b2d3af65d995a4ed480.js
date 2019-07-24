83f1ff4692279ceabe4c10d0a6b233dbe371a08a
BensonLiao <skylock777@gmail.com>
2019-07-24 15:17:19 +0800

    Updates

---
+++ precache-manifest.55dd349049545b2d3af65d995a4ed480.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1ac450b72062bfd9bfa75d2a4b53b2",
+    "url": "/Mazu-Did-Not-Say-That-Shit/index.html"
+  },
+  {
+    "revision": "c487db43fef136bcc6d4",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/js/2.10e91826.chunk.js"
+  },
+  {
+    "revision": "6f4914423c2cb73ffe76",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/js/main.4a358e69.chunk.js"
+  },
+  {
+    "revision": "9c38094a6ad10612b6ee",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/js/runtime~main.503bb5e2.js"
+  },
+  {
+    "revision": "c49fa063a986eea477d31d2e16d6f919",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/blue-check.c49fa063.png"
+  },
+  {
+    "revision": "6aed4496d796625c221b8b3d83ef0e13",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/english.6aed4496.png"
+  },
+  {
+    "revision": "3c65424dc193b0865a30f1710807b863",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/fb-emojis-new.3c65424d.png"
+  },
+  {
+    "revision": "0d754b03b65ee56a0adfebf298ce4c8a",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/fb-emojis.0d754b03.png"
+  },
+  {
+    "revision": "36b90f96a12de5fd63a1822794afedad",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/global.36b90f96.png"
+  },
+  {
+    "revision": "06f0666e8ea60c4ca0487236ee8858bf",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/korean-fish.06f0666e.png"
+  },
+  {
+    "revision": "f320b87a42b8f14fa1c62cbd093d375d",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/land.f320b87a.png"
+  },
+  {
+    "revision": "1b5fa9821cd37c4350a2d817367b202f",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/mazu.1b5fa982.png"
+  },
+  {
+    "revision": "0f1c98c809ad1171a5ac59adbf110804",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/menu.0f1c98c8.png"
+  },
+  {
+    "revision": "e2a06f400841330b4be59c386b792416",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/my-comment.e2a06f40.png"
+  },
+  {
+    "revision": "7b06d1bde5aa937c41260bc5363f8956",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/relevant.7b06d1bd.png"
+  },
+  {
+    "revision": "9c75973b91c578f4522eb8a55d363338",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/responseShare.9c75973b.png"
+  },
+  {
+    "revision": "4a89ef658328f90adaad1e8d884743c2",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/sparkJoy.4a89ef65.png"
+  },
+  {
+    "revision": "7dd2ef4852049cf6351b690333bb8126",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/thumbUp.7dd2ef48.png"
+  },
+  {
+    "revision": "1549640740094a0695f0504aca8fbd71",
+    "url": "/Mazu-Did-Not-Say-That-Shit/static/media/wheel.15496407.png"
+  }
+]);
