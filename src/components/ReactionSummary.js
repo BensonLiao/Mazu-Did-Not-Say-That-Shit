cd80c7159172312ff3be7d98b270f48be984ecb0
BensonLiao <skylock777@gmail.com>
2019-07-30 16:42:21 +0800

    Refactor component ReactionSummary to import proptypes defaults

--- src/components/ReactionSummary.js
+++ src/components/ReactionSummary.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PropTypes } from 'prop-types'
+import PropTypes from 'prop-types'
 import isEqual from 'lodash/isEqual'
 import styled from 'styled-components'
 import { displayFlex, alignCenter } from '../styles'
