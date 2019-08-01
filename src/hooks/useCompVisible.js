59e722975f8f2f8e33aa7a7b6c1049031ae70271
BensonLiao <skylock777@gmail.com>
2019-08-01 17:08:50 +0800

    Fix an issue that onClick not trigger if document.addEventListener's optiion useCapture is true

--- src/hooks/useCompVisible.js
+++ src/hooks/useCompVisible.js
@@ -17,12 +17,17 @@ export default initialIsVisible => {
     }
   }
 
+  // Don't forget to set useCapture = false,
+  // otherwise click from other elements
+  // with click listeners will not work as expected
+  // So here I call: target.addEventListener(type, listener[, useCapture]);
+  // ref: https://developer.mozilla.org/zh-TW/docs/Web/API/EventTarget/addEventListener
   useEffect(() => {
-    document.addEventListener('keydown', handleHideComp, true)
-    document.addEventListener('click', handleClick, true)
+    document.addEventListener('keydown', handleHideComp, false)
+    document.addEventListener('click', handleClick, false)
     return () => {
-      document.removeEventListener('keydown', handleHideComp, true)
-      document.removeEventListener('click', handleClick, true)
+      document.removeEventListener('keydown', handleHideComp, false)
+      document.removeEventListener('click', handleClick, false)
     }
   })
 
